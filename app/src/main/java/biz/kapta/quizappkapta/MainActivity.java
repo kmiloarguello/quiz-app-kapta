5707528af6fc78d3c03c8ad8451812abdafb9d11
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2017-11-17 10:16:50 -0500

    Add colors and improve part 1 of navigation

--- app/src/main/java/biz/kapta/quizappkapta/MainActivity.java
+++ app/src/main/java/biz/kapta/quizappkapta/MainActivity.java
@@ -2,10 +2,13 @@ package biz.kapta.quizappkapta;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.preference.DialogPreference;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -49,9 +52,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(answer1.getText() == mAnswer){
+                    answer1.setBackgroundColor(Color.GREEN);
                     mScore++;
                     score.setText("Score: "+ mScore);
-                    updateQuestion(r.nextInt(mQuestionsLength));
+                    //updateQuestion(r.nextInt(mQuestionsLength));
                 }else{
                     gameOver();
                 }
@@ -62,9 +66,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(answer2.getText() == mAnswer){
+                    answer2.setBackgroundColor(Color.GREEN);
                     mScore++;
                     score.setText("Score: "+ mScore);
-                    updateQuestion(r.nextInt(mQuestionsLength));
+                    //updateQuestion(r.nextInt(mQuestionsLength));
                 }else{
                     gameOver();
                 }
@@ -75,9 +80,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(answer3.getText() == mAnswer){
+                    answer3.setBackgroundColor(Color.GREEN);
                     mScore++;
                     score.setText("Score: "+ mScore);
-                    updateQuestion(r.nextInt(mQuestionsLength));
+                    //updateQuestion(r.nextInt(mQuestionsLength));
                 }else{
                     gameOver();
                 }
@@ -88,9 +94,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(answer4.getText() == mAnswer){
+                    answer4.setBackgroundColor(Color.GREEN);
                     mScore++;
                     score.setText("Score: "+ mScore);
-                    updateQuestion(r.nextInt(mQuestionsLength));
+                    //updateQuestion(r.nextInt(mQuestionsLength));
                 }else{
                     gameOver();
                 }
@@ -102,9 +109,13 @@ public class MainActivity extends AppCompatActivity {
     private void updateQuestion(int num){
         question.setText(mQuestions.getQuestion(num));
         answer1.setText(mQuestions.getChoice(num));
+        answer1.setBackgroundColor(ContextCompat.getColor(getApplicationContext(),R.color.colorPrimary));
         answer2.setText(mQuestions.getChoice2(num));
+        answer2.setBackgroundColor(ContextCompat.getColor(getApplicationContext(),R.color.colorPrimary));
         answer3.setText(mQuestions.getChoice3(num));
+        answer3.setBackgroundColor(ContextCompat.getColor(getApplicationContext(),R.color.colorPrimary));
         answer4.setText(mQuestions.getChoice4(num));
+        answer4.setBackgroundColor(ContextCompat.getColor(getApplicationContext(),R.color.colorPrimary));
 
         mAnswer = mQuestions.getCorrectAnswer(num);
     }
@@ -118,6 +129,7 @@ public class MainActivity extends AppCompatActivity {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                                finish();
                             }
                         }
                 )
@@ -125,6 +137,7 @@ public class MainActivity extends AppCompatActivity {
                         new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
+                                finish();
                             }
                         });
         AlertDialog alertDialog = alertDialogBuilder.create();
